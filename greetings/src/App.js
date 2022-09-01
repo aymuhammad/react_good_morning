dfe55208f401eb34b78bd3325d50fd53bd031489
Auwal Yahaya <auwalyah@yahoo.com>
2022-09-01 17:30:49 +0100

    modified the p tag

--- greetings/src/App.js
+++ greetings/src/App.js
@@ -6,11 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <h1>
-            Greetings 
-          </h1>
-        </p>
+        <p> Greetings </p>
         <a
           className="App-link"
           href="https://reactjs.org"
